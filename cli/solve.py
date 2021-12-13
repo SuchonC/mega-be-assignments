dc03b5f4a29286b53c502b16a3e4f46e8a9035ea
SuchonC <suchon_foke@windowslive.com>
2021-12-13 16:14:56 +0700

    algo v1

---
+++ cli/solve.py
@@ -0,0 +1,18 @@
+def solve(word_list, target):
+    if len(word_list) == 0:
+        return None
+
+    answer = None
+    for i in range(len(word_list)):
+        for j in range(i+1, len(word_list)):
+            if word_list[i] + word_list[j] == target:
+                answer = (word_list[i], word_list[j])
+            elif word_list[j] + word_list[i] == target:
+                answer = (word_list[j], word_list[i])
+    print(answer)
+
+
+if __name__ == "__main__":
+    solve(["ab", "bc", "cd"], "abcd")
+    solve(["ab", "bc", "cd"], "cdab")
+    solve(["ab", "bc", "cd"], "abab")
