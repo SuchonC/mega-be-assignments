0ab0d0a0a2d2f8e981e0df8e976fd47158f3d837
SuchonC <suchon_foke@windowslive.com>
2021-12-14 22:47:32 +0700

    get details function

--- cli/main.py
+++ cli/main.py
@@ -1,25 +1,38 @@
 import click
 import os
 
+from click import utils
+import utils
 # TODO: create a setup.py
 
 
 @click.group()
 def cli():
-    pass
+    web_socket_url = os.getenv('ETHER_NODE_WEBSOCKET_URL')
+    http_url = os.getenv('ETHER_NODE_HTTP_URL')
+    if web_socket_url is None and http_url is None:
+        click.echo(
+            'Either ETHER_NODE_WEBSOCKET_URL or ETHER_NODE_HTTP_URL environment variable needs to be set')
+        exit(1)
 
 
 @cli.command()
 @click.argument('contract_address', type=str)
-def detail():
-    pass
+def detail(contract_address):
+    details = utils.get_contract_detail(contract_address)
+    click.echo(
+        f'Name : {details["name"] if details["name"] is not None else "Function name() is not implemented"}')
+    click.echo(
+        f'Symbol : {details["symbol"] if details["symbol"] is not None else "Function symbol() is not implemented"}')
+    click.echo(
+        f'Decimals : {details["decimals"] if details["decimals"] is not None else "Function decimals() is not implemented"}')
 
 
 @cli.command()
 @click.argument('contract_address', type=str)
 @click.argument('target_address', type=str)
 def balance_of(contract_address, target_address):
-    pass
+    utils.get_balance_of(contract_address, target_address)
 
 
 @cli.command()
--- cli/utils.py
+++ cli/utils.py
@@ -1,1 +1,63 @@
-from web3 import Web3
+import click
+import requests
+import os
+from web3 import Web3, exceptions as web3_exceptions
+
+ETHER_SCAN_MAINNET_URL = 'https://api.etherscan.io'
+w3 = Web3(Web3.WebsocketProvider(os.getenv('ETHER_NODE_WEBSOCKET_URL')))
+
+
+def get_abi(contract_address):
+    payload = {
+        'module': 'contract',
+        'action': 'getabi',
+        'address': contract_address
+    }
+
+    try:
+        response = requests.get(
+            f"{ETHER_SCAN_MAINNET_URL}/api", params=payload)
+    except Exception as e:
+        click.echo(e)
+
+    return response.json()
+
+
+def get_contract_detail(contract_address):
+    contract_address = contract_address.lower()
+    abi_response = get_abi(contract_address)
+
+    # check abi response status
+    if int(abi_response['status']) != 1:
+        click.echo(f'Getting the ABI of {contract_address} failed')
+        click.echo(f'Message : {abi_response["message"]}')
+        click.echo(f'Result : {abi_response["result"]}')
+        exit()
+
+    abi = abi_response['result']
+    checksum_address = Web3.toChecksumAddress(contract_address)
+    contract = w3.eth.contract(abi=abi, address=checksum_address)
+    try:
+        name = contract.functions.name().call()
+    except web3_exceptions.ABIFunctionNotFound:
+        name = None
+
+    try:
+        symbol = contract.functions.symbol().call()
+    except web3_exceptions.ABIFunctionNotFound:
+        symbol = None
+
+    try:
+        decimals = contract.functions.decimals().call()
+    except web3_exceptions.ABIFunctionNotFound:
+        decimals = None
+
+    return {
+        'name': name,
+        'symbol': symbol,
+        'decimals': decimals
+    }
+
+
+def get_balance_of(contract_address, target_address):
+    pass
