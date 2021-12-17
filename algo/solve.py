fbddcc7d2886deb7c0ec2e0b737ed48f7e2a1f08
SuchonC <suchon_foke@windowslive.com>
2021-12-17 16:04:46 +0700

    v2

--- algo/solve.py
+++ algo/solve.py
@@ -1,24 +1,37 @@
 def solve(word_list, target):
     if len(word_list) == 0:
         return None
+    if len(target) == 0:
+        return None
 
-    target_length = len(target)
-    answer = None
+    found = {}
 
-    for i in range(len(word_list)):
-        if len(word_list[i]) >= target_length:
-            continue
-        for j in range(i+1, len(word_list)):
-            if len(word_list[j]) >= target_length:
-                continue
-            if word_list[i] + word_list[j] == target:
-                answer = (word_list[i], word_list[j])
-            elif word_list[j] + word_list[i] == target:
-                answer = (word_list[j], word_list[i])
-    print(answer)
+    for w in word_list:
+        # if w is the head of the target
+        if w == target[:len(w)]:
+            tail = target[len(w):]
+            # if its tail is already found, then return
+            if tail in found:
+                return (w, tail)
+            # else mark the head as found
+            found[w] = True
+        # if w is the tail of the target
+        elif w == target[len(w):]:
+            head = target[:len(w)]
+            # if its head is already found, then return
+            if head in found:
+                return (head, w)
+            # else mark the tail as found
+            found[w] = True
+
+    return None
 
 
-if __name__ == "__main__":
-    solve(["ab", "bc", "cd"], "abcd")
-    solve(["ab", "bc", "cd"], "cdab")
-    solve(["ab", "bc", "cd"], "abab")
+def main():
+    word_list = input('Type input words separated by space (ie. ab bc cd): ')
+    target = input('Type the target word: ')
+    print('Answer:', solve(word_list.split(), target))
+
+
+if __name__ == '__main__':
+    main()
